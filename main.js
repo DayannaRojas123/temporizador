c5c3594bea29ee3cf9704cca4dd62826071e4005
DayannaRojas123 <112741251+DayannaRojas123@users.noreply.github.com>
2022-09-27 14:49:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,84 +1,20 @@
 boton1=document.getElementById('calcular')
-
-
-
-
-
 boton1.addEventListener('click',
 iniciar)
-
-
-
-
 function iniciar(){
-
-
-
-
-    let segundos = document.getElementById('tiempo').value
-
+	let segundos = document.getElementById('tiempo').value
     temporizador(segundos)
-
-
-
-    
-
-
-
-
-
 function temporizador(segundos) {
-
-
-
-
-    
-    if(segundos ==
-    'NaN'  )
-    
-    {
-    
-        alert('ingrese caracter numerico')
+		if(segundos===0){
+			document.getElementById('resultado').innerText ='finish'
+		}if(segundos<0){
+			alert('por favor ingrese numeros mayores a cero')
         location. reload()
-    
-    }
-        
-    
-
-
-
-
-	
-    if(segundos===0){
-         
-        document.getElementById('resultado').innerText ='finish'
-        
-    }
-
-   
-
-    if(segundos<0){
-
-        alert('por favor ingrese numeros mayores a cero')
-        location. reload()
-
-
-    }
-
-    else{
-
-
-document.getElementById('resultado').innerText =segundos
-setTimeout(temporizador,1000,segundos-1)
-       
-    }
-
-
-
-    
-
-
-    
-}
-
+		}else{
+			document.getElementById('resultado').innerText =segundos
+setTimeout(temporizador,
+			1000,
+			segundos-1)
+		}
+	}
 }
